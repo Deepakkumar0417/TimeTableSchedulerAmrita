a4564b8b428288a906201aa2bb8fe2bf972126e6
Deepakkumar0417 <144591562+Deepakkumar0417@users.noreply.github.com>
2025-08-26 13:05:07 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:5173';
+const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:5173',"https://timetable-backend.onrender.com";
 
 const corsOptions = {
   origin: CLIENT_URL,
@@ -56,4 +56,5 @@ const start = async () => {
 };
 
 start();
+
 
